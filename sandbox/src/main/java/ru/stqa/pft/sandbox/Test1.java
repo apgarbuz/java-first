917e190aa5ed6a14319178521feecac2e4112c49
apgarbuz <apgarbuz@gmail.com>
2025-02-26 12:51:08 +0100

    ,
    ,,

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Test1.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Test1.java
@@ -1,7 +1,19 @@
 package ru.stqa.pft.sandbox;
 
 public class Test1 {
+
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		hello("world!");
+		hello("user!");
+		hello("Luser!!");
+
+		Square s = new Square(5);
+		System.out.println("Площадь квадрата со стороной" + s.1 + "=" + s.area);
+
+		rectangle r = new Rectangle(4,6);
+		System.out.println("площадь прямоугольника со сторонами" + r.a + "и" + r.b + "=" + r.area());
+
 	}
+	public static void hello (String somebody) {
+		System.out.println("Hello," + somebody + "!");}
 }
